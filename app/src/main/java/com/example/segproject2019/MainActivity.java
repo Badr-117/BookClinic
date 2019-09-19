5962efc3f1b05b217616c7e782def85405eba02e
BBK447 <fboud005@uottawa.ca>
2019-09-19 09:37:29 -0400

    J'ai ajouté le bouton

--- app/src/main/java/com/example/segproject2019/MainActivity.java
+++ app/src/main/java/com/example/segproject2019/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.segproject2019;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+	private button1 // bouton ajouter par farid
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
