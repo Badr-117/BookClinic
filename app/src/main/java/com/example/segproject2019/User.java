c6c2570e56e08c908e12ccf6f1507cd658389abc
Badr-117 <ibrahimi-badr@hotmail.com>
2019-10-19 12:32:00 -0400

    working version

--- app/src/main/java/com/example/segproject2019/User.java
+++ app/src/main/java/com/example/segproject2019/User.java
@@ -1,4 +1,18 @@
 package com.example.segproject2019;
 
 public class User {
+
+    public String username,userEmail, password, userType;
+
+    public User(){
+
+    }
+
+    public User(String username,String userEmail,String password,  String userType){
+        this.username = username;
+        this.userEmail = userEmail;
+        this.password = password;
+        this.userType = userType;
+    }
+
 }
